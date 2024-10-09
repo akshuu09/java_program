2ee620aae78548da1920cbd974d0770dff3762c8
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-10-09 10:13:46 +0530

    Add files via upload

---
+++ java/methods/Example1.java
@@ -0,0 +1,11 @@
+class 
+{
+	public static void main(String args[])
+	{
+		String op=generate();
+		System.out.println(op);
+		int op=generateOtp();
+		System.out.println(op);
+	}
+	public static  String generate
+}
