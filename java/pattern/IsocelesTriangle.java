f53bdec6b71390cb2310ba81ac470dae4c4be7bc
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-11-01 20:25:47 +0530

    Add files via upload

---
+++ java/pattern/IsocelesTriangle.java
@@ -0,0 +1,29 @@
+class IsocelesTriangle
+{
+	public static void main(String args[])
+	{
+		/*
+		
+		 *
+	   * * *
+	 * * * * *
+   * * * * * * *
+   
+   */
+       for(int i=1;)
+		{
+			for(int space=1; space<=i; space++)
+			{
+				System.out.print(" ");
+				for()
+				{
+					
+				}
+			}
+		}
+  
+  
+  
+	}
+}
+
