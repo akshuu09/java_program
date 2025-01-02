00999ed2c7c2c8c92f642e020050302f9577e7bf
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2025-01-02 12:41:58 +0530

    Add files via upload

---
+++ Arrays/ArithmeticOperation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class ArithmeticOperation
+{
+	public static void main(String[] args) 
+	{
+	   int num1=Integer.parseInt(args[0]);
+	   int num2=Integer.parseInt(args[1]);	
+	   add();
+	   sub();
+	   multi();
+
+	}
+	 public static int add(String [] args)
+	 {
+        
+	 }
+	 public static int sub(String [] args)
+	 {
+	 }
+	 public static int multi(String [] args)
+	 {
+            return String;
+	 }
+}
