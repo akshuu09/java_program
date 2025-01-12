dfb3edbff0b07ce81ce94dc444041ac12eb66885
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2025-01-12 19:27:49 +0530

    Add files via upload

---
+++ Arrays/StringExample.java
@@ -0,0 +1,32 @@
+import java.util.*;
+classs StringExample
+{
+	public static void main(String[] args) 
+	{
+		String str="hello";
+		char [] arr = new char[str.length()];
+		System.out.println(str);
+		for (int i=; ; ) {
+			
+		}
+		for(int i=0; i<a.length; i++)
+		{
+			int cnt = 0;
+			for(int j=0; j< a.length; j++)
+			{
+				if(a[i]==a[j] && b[j]==false)
+				{
+					cnt++;
+					b[j]=true;
+				}
+				
+			}
+		
+		if(cnt >0
+	)
+		{
+			System.out.println(a[i] + " " + cnt );
+		}
+		}
+	}
+}
